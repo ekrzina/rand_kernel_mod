1802e31b0074248f47c4a50ba18611f2d3901405
Elena Kržina <72581350+ekrzina@users.noreply.github.com>
2024-09-22 17:59:52 +0200

    Add files via upload

--- rest-api-go/cmd/main.go
+++ rest-api-go/cmd/main.go
@@ -4,7 +4,8 @@ import apiserve "rand_kernel_mod/pkg/api"
 
 func main() {
 	var apiHandler = apiserve.ApiHandler{
-		Port: "2315",
+		Port:     "2315",
+		FileName: "/proc/rngen",
 	}
 	apiHandler.HandleRequests()
 }
