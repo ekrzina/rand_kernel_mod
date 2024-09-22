267d40208fc1c290cc530bf66ab6aeb06a756f09
Elena Kržina <72581350+ekrzina@users.noreply.github.com>
2024-09-22 16:58:06 +0200

    Update rngen.c

--- rng_module_c/rngen.c
+++ rng_module_c/rngen.c
@@ -15,14 +15,21 @@ static struct proc_ops proc_fops = {
 // function for generating and returning a random number, used with profcs
 // proc filesystem can be read multiple times by user-space -> offset keeps track -> no need for 'for' loop
 static ssize_t random_number_gen(struct file *file, char __user *buf, size_t count, loff_t *offset) {
+    
     unsigned int random_number;
     char buffer[32];
     ssize_t len;
+
+    if (*offset > 0) {
+      return 0;
+    }
 
     get_random_bytes(&random_number, sizeof(random_number));
     // converts to string for profcs
     len = snprintf(buffer, sizeof(buffer), "%u\n", random_number);
     
+    printk(KERN_INFO "rngen: Generated random number: %u\n", random_number);
+
     // check if all data is read
     if (*offset >= len) {
         return 0; // EOF
@@ -33,6 +40,8 @@ static ssize_t random_number_gen(struct file *file, char __user *buf, size_t cou
         return -EFAULT;
     }
     
+    printk(KERN_INFO "rngen: Successfully returned random number to user\n");
+
     // update after one read 
     *offset += len;
     return len;
@@ -40,13 +49,13 @@ static ssize_t random_number_gen(struct file *file, char __user *buf, size_t cou
 
 static int __init rn_init(void) {
     proc_create(PROC_NAME, 0, NULL, &proc_fops);
-    printk(KERN_INFO "Loaded random number module.\n");
+    printk(KERN_INFO "rngen: Loaded random number module.\n");
     return 0; // Return success
 }
 
 static void __exit rn_exit(void) {
     remove_proc_entry(PROC_NAME, NULL);
-    printk(KERN_INFO "Exiting random number module.\n");
+    printk(KERN_INFO "rngen: Exiting random number module.\n");
 }
 
 MODULE_LICENSE("GPL");
